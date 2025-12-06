aa4f55bcde05c282157fe17e350813aab95b0df5
Samuel Hållman <samuel.hollman@hotmail.com>
2025-12-06 12:48:45 +0200

    ES Module test!

--- import.js
+++
@@ -1,2 +0,0 @@
-const exportFile = require("./export");
-console.log(exportFile.add(2, 2));
---
+++ import.mjs
@@ -0,0 +1,3 @@
+import { add, subtract } from "./export.mjs";
+console.log("add: " + add(2, 5));
+console.log("subtract: " + subtract(10, 6));
