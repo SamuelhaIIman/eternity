7c99a333d1c657a939d46e8f3fc7ad5e970b47de
Samuel Hållman <samuel.hollman@hotmail.com>
2025-12-14 19:37:20 +0200

    Tests!

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 // Import the HTTP module
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
-
   fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(500);
@@ -24,3 +21,13 @@ const PORT = 3000;
 server.listen(PORT, 'localhost', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/html' })
+//   fs.createReadStream('index.html').pipe(res)
+// })
+
+// server.listen(process.env.PORT || 3000)
