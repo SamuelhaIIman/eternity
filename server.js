808b08dda613d569562563a2cd80403b734ab247
Samuel Hållman <samuel.hollman@hotmail.com>
2025-12-15 18:56:36 +0200

    Connection made to index.html and the testing for buyer has been started.

--- server.js
+++ server.js
@@ -3,31 +3,24 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  fs.readFile(filePath, (err, data) => {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  fs.readFile("./public/index.html", (err, data) => {
     if (err) {
-      res.writeHead(500);
-      res.end('Error loading file');
-      return;
+      res.writeHead(404)
+      res.write("Error: File Not Found")
+    } else {
+      res.write(data)
     }
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+    res.end()
+  })
 });
 
-// Define the port to listen on const PORT = 3000;
+
 const PORT = 3000;
-// Start the server and listen on the specified port
-server.listen(PORT, 'localhost', () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log("Something went wrong!", err)
+  } else {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  }
 });
-
-// const http = require('http')
-// const fs = require('fs')
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'content-type': 'text/html' })
-//   fs.createReadStream('index.html').pipe(res)
-// })
-
-// server.listen(process.env.PORT || 3000)
