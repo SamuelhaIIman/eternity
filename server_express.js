eb365307500c4895675f954bba03f74a78648de5
Samuel Hållman <samuel.hollman@hotmail.com>
2025-12-17 19:47:57 +0200

    Testing the express 404 error handling and ejs installation.

--- server_express.js
+++ server_express.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
     res.send("Hello World!")
 });
 
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+
+app.use((req, res) => {
+  res.status(404).send('404 - Page not found');
+});
+
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 });
