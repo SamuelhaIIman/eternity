3e27fdfb27ef861bcf8e762a5bcf420911224b41
Samuel Hållman <samuel.hollman@hotmail.com>
2025-12-18 19:48:48 +0200

    Connection to CSS file and more testing with express folder and file directory structure.

--- server_express.js
+++ server_express.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port  = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-});
 
 app.set("view engine", "ejs");
-app.use(express.static("public"))
+
+app.set("views", "./views")
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index")
+});
 
 app.use((req, res) => {
   res.status(404).send('404 - Page not found');
